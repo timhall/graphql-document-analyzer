a7336c63933e073052d4c9f05ef8b11e570c3be4
Tim Hall <tim.hall.engr@gmail.com>
2022-10-21 20:51:37 -0400

    Include relevant types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 export { analyze } from "./analyze";
 export { interpolate } from "./interpolate";
 export { print } from "./print";
+export { isExtendedDocumentNode } from "./extended-ast";
+
+export type {
+  ExtendedASTNode,
+  ExtendedDocumentNode,
+  SectionNode,
+  InvalidDefinitionNode,
+  InvalidOperationDefinitionNode,
+  InvalidFragmentDefinitionNode,
+  IgnoredNode,
+} from "./extended-ast";
